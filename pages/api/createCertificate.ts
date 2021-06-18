c4eebd643e87aed1224286a8b2f4c6c1082c0662
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-06-19 02:27:05 +0530

    🐛

--- pages/api/createCertificate.ts
+++ pages/api/createCertificate.ts
@@ -9,7 +9,7 @@ import AWS from "aws-sdk";
 AWS.config.update({
   accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_SECRET,
-  region: process.env.REGION,
+  region: "ap-south-1",
 });
 export default async function createCertificate(
   req: NextApiRequest,
