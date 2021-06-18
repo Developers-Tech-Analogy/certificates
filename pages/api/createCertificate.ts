3ddb25ee74672f45ea61ba1a29c7fea4d9ddba77
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-06-18 20:04:40 +0530

    🐛 Fix mailer

--- pages/api/createCertificate.ts
+++ pages/api/createCertificate.ts
@@ -24,53 +24,66 @@ export default async function createCertificate(
         message: "Invalid Input",
       };
     } else {
-      try{
+      try {
         const certificateUrl: string =
-        "https://s3.ap-south-1.amazonaws.com/org.techanalogy.certificates/Templates/cert-v1.png";
-      const { data, error } = await supabase
-        .from("certificate-details")
-        .select()
-        .eq("email", value.email)
-        .eq("eventName", value.eventName);
+          "https://s3.ap-south-1.amazonaws.com/org.techanalogy.certificates/Templates/cert-v1.png";
+        const { data, error } = await supabase
+          .from("certificate-details")
+          .select()
+          .eq("email", value.email)
+          .eq("eventName", value.eventName);
         console.log(data);
-      if (error || data.length === 0) {
-          console.log('Error');
-          
-          console.log(error)
+        if (error || data.length === 0) {
+          console.log("Error");
+
+          console.log(error);
           res.status(404).json({
             message: "Not found",
           });
           return;
-      }
-      let imageResult;
-      const fileName = `${data[0].name.replace(/\s/g, '')}-${value.eventName.replace(/\s/g, '')}`;
-      console.log(fileName)
-      Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then((font) => {
-        Jimp.read(certificateUrl)
-          .then((image) => {
-            image.print(font, 800, 600, data[0].name);
-            let file =
-              `./public/certificates/${fileName}.` +
-              image.getExtension();
-            image.write(file);
+        }
+        let imageResult;
+        const fileName = `${data[0].name.replace(
+          /\s/g,
+          ""
+        )}-${value.eventName.replace(/\s/g, "")}`;
+        console.log(fileName);
+        Jimp.loadFont(Jimp.FONT_SANS_64_BLACK)
+          .then((font) => {
+            Jimp.read(certificateUrl)
+              .then((image) => {
+                image.print(font, 800, 600, data[0].name);
+                let file =
+                  `./public/certificates/${fileName}.` + image.getExtension();
+                image.write(file);
+              })
+              .catch((error) => {
+                throw {
+                  status: 500,
+                  message: "Error while reading image",
+                };
+              });
           })
           .catch((error) => {
+            console.log(error);
             throw {
               status: 500,
-              message: "Error while reading image",
+              message: "Error in loading font",
             };
           });
-      });
-      //mailgun(value.email, 'Our minions have got a parcel for you', certificate(`${process.env.HOST}/certificates/${fileName}.png`))
-      res.status(201).json({
-        message: "Get Certificate",
-      });
-      }
-      catch(e){
+        mailgun(
+          value.email,
+          "Our minions have got a parcel for you",
+          certificate(`${process.env.HOST}/certificates/${fileName}.png`)
+        );
+        res.status(201).json({
+          message: "Get Certificate",
+        });
+      } catch (e) {
         console.log(e);
-    res.status(e.status || 500).json({
-      message: e.message || "An unexpected error occurred",
-    });  
+        res.status(e.status || 500).json({
+          message: e.message || "An unexpected error occurred",
+        });
       }
     }
   } catch (e) {
