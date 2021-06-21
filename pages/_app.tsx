360d32dbb95d08f53142038c62812a22e0a2f585
sbiswas2209 <sbiswas2209@outlook.com>
2021-06-21 22:34:37 +0530

    ➕ Add Customerly and Billboard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,31 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
+import { customerly } from "react-customerly";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    customerly.initialize(process.env.NEXT_PUBLIC_CUSTOMERLY_ID, {
+      widget_position: "right",
+      widget_color: "06d6a0",
+    });
+  }, []);
   return (
     <div>
       <Head>
         <title>Certificates @ Tech Analogy</title>
         <meta name="description" content="Reap the rewards of your hard work" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="author" content="Tech Analogy" />
+        <meta property="og:url" content="https://techanalogy.org" />
+        <meta property="og:title" content="Careers @ Tech Analogy" />
+        <meta property="og:description" content="Reap the rewards of your hard work" />
+        <meta
+          property="og:image"
+          content="https://billboard.srmkzilla.net/api/blog?title=Certificates&subtitle=Tech%20Analogy&theme=dark"
+        />
       </Head>
       <ChakraProvider>
         <Component {...pageProps} />
